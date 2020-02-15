2eecd6b4c92c8cf83c8aba0e00c33b00b81a4a0e
simyeongryu <r.simyeong@gmail.com>
2020-02-15 12:33:01 +0900

    canvas pixel manipulating size 설정

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+/** search canvas on MDN
+ * HTML element. context를 갖는다. 
+ * context란 해당 요소의 픽셀에 접근할 수 있는 방법. 우리는 context로 canvas 의 픽셀을 다룰 수 있다.
+ * canvas는 두 개의 사이즈를 가져야 한다.
+ * 1) CSS, HTML로 정해주는 사이즈.
+ * 2) pixel을 다룰 수 있는 공간의 크기.
+ */
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+// 2) pixel을 다룰 수 있는 공간의 크기.
+canvas.width = 700;
+canvas.height = 700;
+
+/** canvas context 기본값 설정 */
+ctx.strokeStyle = "black"; // 사용자가 처음 사용하는 색.
+ctx.lineWidth = 2.5; // 펜의 굵기
 
 // 그려지는 상태인지 아닌지.
 let painting = false;
 
 function stopPainting() {
     painting = false;
+}
+
+function startPainting() {
+    paining = true;
 }
 
 function onMouseMove(event) {
@@ -16,28 +36,32 @@ function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     // console.log(event, x, y); 값 확인
+    if (!painting) {
+        ctx.beginPath(); // 클릭한 지점부터 path 시작
+        ctx.moveTo(x, y); // x, y로 지점 이동
+        // 이동 이후엔 더이상 위의 구문은 작동하지 않는다. 
+    } else {
+        ctx.lineTo(x, y); // beginPath()부터 이동된 x, y까지 선으로 연결
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
     painting = true;
-    
-}
-
-function onMouseUp (event) {
-    stopPainting()
-    
 }
 
 if (canvas) {
     /* canvas 위에서 마우스가 움직이면 감지한다. */
     canvas.addEventListener("mousemove", onMouseMove);
     /** canvas 클릭하는 순간을 감지한다. */
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     /** canvas 클릭을 해제하는 순간을 감지한다. */
-    canvas.addEventListener("mouseup", onMouseUp);
+    // onMouseUp 함수를 만드는 것보다 stopPainting을 재활용하는 것이 좀더 효율적이다.
+    canvas.addEventListener("mouseup", stopPainting);
     /** canvas 영역을 떠나는 순간을 감지한다. */
     // onMouseLeave 함수를 만드는 것보다 stopPainting을 재활용하는 것이 좀더 효율적이다.
     canvas.addEventListener("mouseleave", stopPainting);
-
 }
+
+
 
